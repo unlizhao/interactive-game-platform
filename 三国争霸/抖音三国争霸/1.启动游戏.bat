f716a038655c7481602e477b6dd6489d011d662c
深海大冒险 <1975125565@qq.com>
2022-06-30 20:19:35 +0800

    三国MOD版上线
    替代原游戏，可以自定义更多内容

--- 三国争霸/抖音三国争霸/1.启动游戏.bat
+++
@@ -1,3 +0,0 @@
-@echo off&cd %~dp0&color 0a
-echo %cd%
-start cmd /k DYSanGuoGame.exe
--- 三国争霸/抖音三国争霸宽屏高清版/2.直播监听.bat
+++
@@ -1,4 +0,0 @@
-@echo off&cd %~dp0&color 0a
-echo %cd%
-set /p url=请输入抖音直播间ID(一长串数字):
-start cmd /k LiveMonitor %url%
